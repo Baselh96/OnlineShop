44060e0eb5c514037a2fb6bd3c46bc9bf6fee9fc
Baselh96 <husseinbasel14@gmail.com>
2022-07-23 21:24:40 +0200

    creating profilpage

--- src/localData/UserData.js
+++ src/localData/UserData.js
@@ -2,6 +2,5 @@ export const UserData = {
     "id": 1,
     "name": "Basel",
     "email": "husseinbasel14@gmail.com",
-    "description": "Universidad del Tepeyac",
-    "password": "1234"
+    "password": "12345"
   };
--- src/pages/profil/index.js
+++ src/pages/profil/index.js
@@ -12,19 +12,32 @@ const Alert = React.forwardRef(function Alert(props, ref) {
 
 export default function ProfilPage() {
   const [user, setUser] = useState(UserData);
-  const [open, setOpen] = React.useState(false);
+  const [openForSuccess, setOpenForSuccess] = React.useState(false);
+  const [openForError, setOpenForError] = React.useState(false);
+  const [repeatedPassword, setRepeatedPassword] = React.useState(user.password);
+  const [error, setError] = React.useState("");
   
 
   const handleClick = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
+    if (user.name.length === 0) {
+      setError("Sie haben keinen Namen eingegeben!");
+      setOpenForError(true);
+    } else if (user.email.length === 0) {
+      setError("Sie haben keine E-Mailadresse eingegeben!");
+      setOpenForError(true);
+    } else if (!user.email.includes("@")) {
+      setError("Ihre E-Mailadresse ist nicht korrekt!");
+      setOpenForError(true);
+    } else if (user.password.length <= 4) {
+      setError("Ihr Passwort muss mindestens 5 Zeichen haben!");
+      setOpenForError(true);
+    } else if (user.password !== repeatedPassword) {
+      setError("Ihre Passwörter stimmen nicht überein!");
+      setOpenForError(true);
+    } else {
+      console.log(user)
+      setOpenForSuccess(true);
     }
-
-    setOpen(false);
   };
 
 
@@ -32,25 +45,32 @@ export default function ProfilPage() {
     <div position="static" className='profil-container'>
       <div className='profil-container-item'>
         <span className='label'>Name: </span>
-        <TextField className='textfield'required label="Required" defaultValue= {user.name} />        
+        <TextField className='textfield' onChange={(e) => user.name = e.target.value} required label="Required" defaultValue= {user.name} />        
       </div>
       <div className='profil-container-item'>
         <span className='label'>E-Mail: </span>
-        <TextField className='textfield' type="email" required label="Required" defaultValue= {user.email} /> 
+        <TextField className='textfield' type="email" onChange={(e) => user.email = e.target.value} required label="Required" defaultValue= {user.email} /> 
       </div>
       <div className='profil-container-item'>
         <span className='label'>Passwort: </span>
-        <TextField className='textfield' type="password" required label="Required" defaultValue= {user.password} />        
+        <TextField className='textfield' type="password" onChange={(e) => user.password = e.target.value} required label="Required" defaultValue= {user.password} />        
       </div>
       <div className='profil-container-item'>
         <span className='label'>Passwortwiederholung: </span>
-        <TextField className='textfield' type="password" required label="Required" defaultValue= {user.password} />      
+        <TextField className='textfield' type="password" onChange={(e) => setRepeatedPassword(e.target.value)} required label="Required" defaultValue= {user.password} />      
       </div>
       <div className='profil-container-item'>
         <Button variant="contained" onClick={handleClick}>SPEICHERN</Button>
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-          <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-            This is a success message!
+
+        <Snackbar open={openForSuccess} autoHideDuration={3000} onClose={() => setOpenForSuccess(false)}>
+          <Alert onClose={() => setOpenForSuccess(false)} severity="success" sx={{ width: '100%' }}>
+            Ihre Daten sind gespeichert!
+          </Alert>
+        </Snackbar>
+
+        <Snackbar open={openForError} autoHideDuration={3000} onClose={() => setOpenForError(false)}>
+          <Alert onClose={() => setOpenForError(false)} severity="error" sx={{ width: '100%' }}>
+            {error}
           </Alert>
         </Snackbar>
       </div>
